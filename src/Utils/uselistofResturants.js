683afa0f0e88cab13bede2ebc4e80b7a13d0d130
Gautam Aggarwal-05 <aggarwalgautam06@gmail.com>
2024-11-23 00:25:27 +0530

    Update

--- src/Utils/uselistofResturants.js
+++ src/Utils/uselistofResturants.js
@@ -10,7 +10,7 @@ const uselistofResturant = ()=>{
     //fetching live data and puttind it in listofResturant
     const fetchData = async ()=>{
         const data = await fetch(
-            `${process.env.BACKEND_BASEURL}/api/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`);
+            "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
     const json = await data.json(); //await data.json();
         setlistofResturant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         }
